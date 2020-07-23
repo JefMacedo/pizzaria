e13334edca7f02144803079594beb9cc58413bfe
Jeferson Macedo <jhef.salles@gmail.com>
2020-07-23 09:37:29 -0400

    add as imagens das pizzas, preço, nome e descrição

--- script.js
+++ script.js
@@ -7,5 +7,11 @@ pizzaJson.map((item, index) => {
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
     //preenchendo as informações em pizza-item
 
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
     c('.pizza-area').append(pizzaItem);
 });
