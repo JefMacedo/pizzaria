e629f40d1dea17ff4303786b269f57fe9522db96
Jeferson Macedo <jhef.salles@gmail.com>
2020-07-23 14:22:48 -0400

    adicionando os itens no carrinho

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+let cart = [];
 let modalQt = 1;
+let modalKey = 0;
 
 const c = (el) => document.querySelector(el); // retorna apenas o item
 const cs = (el) => document.querySelectorAll(el); // retorna um array com os itens
@@ -16,11 +18,11 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
 
     pizzaItem.querySelector('a').addEventListener('click', (e) => {
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
         e.preventDefault();
 
         modalQt = 1;
-
-        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        modalKey = key;
 
         c('.pizzaBig img').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
@@ -55,6 +57,48 @@ function closeModal() {
         c('.pizzaWindowArea').style.display = 'none';
     }, 500);
 }
+
 cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
     item.addEventListener('click', closeModal);
-})
+});
+
+c('.pizzaInfo--qtmenos').addEventListener('click', () => {
+    if (modalQt > 1) {
+        modalQt--;
+        c('.pizzaInfo--qt').innerHTML = modalQt;
+    }
+});
+
+c('.pizzaInfo--qtmais').addEventListener('click', () => {
+    modalQt++;
+    c('.pizzaInfo--qt').innerHTML = modalQt;
+});
+
+cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+    size.addEventListener('click', (e) => {
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+    });
+});
+
+c('.pizzaInfo--addButton').addEventListener('click', () => {
+
+    let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
+
+    let identifier = pizzaJson[modalKey].id + '@' + size;
+    let key = cart.findIndex((item) => item.identifier == identifier);
+
+    if (key > -1) {
+        cart[key].qt += modalQt;
+    } else {
+        cart.push({
+            identifier,
+            id: pizzaJson[modalKey].id,
+            size,
+            qt: modalQt
+        });
+    }
+
+
+    closeModal();
+});
